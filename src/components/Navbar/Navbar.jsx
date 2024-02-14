6a2cb391eac1292fb4e260629e43687525bf167b
Helam7u7 <helam053@gmail.com>
2024-02-14 12:39:27 -0500

    menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
 import { Link,NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+  
+  const [menuOpen, setMenuOpen] = useState(true)
+
   return (
     <nav className="navbar">
       <div className="navbar-logo"><Link to='/'><img src="/svg/logo.png" alt="" /></Link></div>
-      <div className='menu'>
+      <div 
+        className='menu'
+        onClick={() => {setMenuOpen(!menuOpen)}}
+      >
         <img src="/svg/menu.svg" alt="menu" />
       </div>
-      <ul className="navbar-links">
-        <li><NavLink to='/' className="navbar-link">Inicio</NavLink></li>
-        <li><NavLink to='/nosotros' className="navbar-link">Nosotros</NavLink></li>
-        <li><NavLink to='/servicios' className="navbar-link">Servicios</NavLink></li>
-        <li><NavLink to='/contacto' className="navbar-link">Contacto</NavLink></li>
+      <ul className={["navbar-links", menuOpen ? "open": ""].join(" ")}>
+        <li className='nav-link'><NavLink to='/' className="navbar-link">Inicio</NavLink></li>
+        <li className='nav-link'><NavLink to='/nosotros' className="navbar-link">Nosotros</NavLink></li>
+        <li className='nav-link'><NavLink to='/servicios' className="navbar-link">Servicios</NavLink></li>
+        <li className='nav-link'><NavLink to='/contacto' className="navbar-link">Contacto</NavLink></li>
       </ul>
     </nav>
   )
