ca7063ad0ccedfefb926f177a650dd4ce1fdc34e
Helam7u7 <helam053@gmail.com>
2024-02-14 23:58:18 -0500

    end

--- src/Pages/PageServices/PageServices.jsx
+++ src/Pages/PageServices/PageServices.jsx
@@ -10,9 +10,9 @@ const PageServices = () => {
     </main> 
     <section className='section-services section'>
       <div className='services-grid'>
-        <div className='services-grid-item'>
+        <div className='services-grid-item item-blue'>
           <div className='services-item-head'>
-            <div className='services-item-img'><img src="/servicios/license.png" alt="" /></div>
+            <div className='services-item-img'><div className='img-service'></div></div>
             <h2>Consultorías en Seguridad y Salud Ocupacional</h2>
           </div>
           <ul className='services-item-ul'>
@@ -22,9 +22,9 @@ const PageServices = () => {
             <li>Implementación del Programa Total Worker Health</li>
           </ul>
         </div>
-        <div className='services-grid-item'>
+        <div className='services-grid-item item-yellow'>
           <div className='services-item-head'>
-            <div className='services-item-img'><img src="/servicios/stadistics.png" alt="" /></div>
+            <div className='services-item-img'><div className='img-service'></div></div>
             <h2>Implementación del Programa Lean Mining</h2>
           </div>
           <ul className='services-item-ul'>
@@ -33,6 +33,56 @@ const PageServices = () => {
             <li>Gestión de alianzas estratégicas con empresas contratistas.
             </li>
             <li>Prevención de eventos mayores, fatales y graves.</li>
+          </ul>
+        </div>
+        <div className='services-grid-item item-blue'>
+          <div className='services-item-head'>
+            <div className='services-item-img'><div className='img-service'></div></div>
+            <h2>Cultura y Liderazgo en Seguridad</h2>
+          </div>
+          <ul className='services-item-ul'>
+            <li>Diagnóstico de la línea de base organizacional.</li>
+            <li>
+Implementación y desarrollo de una Cultura de Seguridad de una empresa.</li>
+            <li>Medición de la evolución y madurez del clima y cultura de Seguridad.</li>
+            <li>Auditorias integrales del Sistema de Gestión y Cultura de Seguridad.</li>
+          </ul>
+        </div>
+        <div className='services-grid-item item-yellow'>
+          <div className='services-item-head'>
+            <div className='services-item-img'><div className='img-service'></div></div>
+            <h2>Entrenamiento y Certificación</h2>
+          </div>
+          <ul className='services-item-ul'>
+            <li>Programas de aprendizaje y entrenamiento para lideres mineros.</li>
+            <li>Programas de certificación en riesgos críticos.</li>
+            <li>Talleres de Liderazgo Visible de Seguridad.
+            </li>
+            <li>Programas de entrenamiento de seguridad e higiene industrial</li>
+          </ul>
+        </div>
+        <div className='services-grid-item item-blue'>
+          <div className='services-item-head'>
+            <div className='services-item-img'><div className='img-service'></div></div>
+            <h2>Factores Humanos y Organizativos de la Seguridad</h2>
+          </div>
+          <ul className='services-item-ul'>
+            <li>Programas de Seguridad Basado en el Comportamiento de las Organizaciones.</li>
+            <li>Programa de Conductas seguras.</li>
+            <li>Ingeniería de la Resiliencia en la Investigación de Accidentes.</li>
+            <li>Seguridad de procesos</li>
+          </ul>
+        </div>
+        <div className='services-grid-item item-yellow'>
+          <div className='services-item-head'>
+            <div className='services-item-img'><div className='img-service'></div></div>
+            <h2>Estándares ESG</h2>
+          </div>
+          <ul className='services-item-ul'>
+            <li>Marco de madurez de los estándares ESG.</li>
+            <li>Regulaciones y normas internacionales del ESG.</li>
+            <li>Integración del ESG a la excelencia Operacional.
+            </li>
           </ul>
         </div>
       </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -29,9 +29,9 @@ const Footer = () => {
             </div>
           </div>
           <div className='contacto-item'>
-            <div className='contacto-item-img'><img src="/svg/facebookIcon.svg" alt="" /></div>
+            <div className='contacto-item-img'><a href="https://www.facebook.com/segguroconsultingsac" target='_blank'><img src="/svg/facebookIcon.svg" alt="facebook" /></a></div>
             <div className='contacto-item-img'><img src="/svg/instagramIcon.svg" alt="" /></div>
-            <div className='contacto-item-img'><img src="/svg/linkedinIcon.svg" alt="" /></div>
+            <div className='contacto-item-img'><a href="https://www.linkedin.com/in/segguro-consulting-36067a135/" target="_blank" rel="noopener noreferrer"><img src="/svg/linkedinIcon.svg" alt="" /></a></div>
             <div className='contacto-item-img'><img src="/svg/twitterIcon.svg" alt="" /></div>
           </div>
         </div>
